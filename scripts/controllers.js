ddab9571f8b30f96b052681fcdd678a0145f307b
Gabe Villalobos <gabriel_villalobos@outlook.com>
2015-06-10 19:50:41 -0700

    Added initial prototype

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -16,21 +16,18 @@ sodecApp.service('deviceService', function($http) {
 
 sodecApp.controller('temperatureController', function($scope, deviceService) {
     var TempSensors = [
-        {name: 'Living Room', device : 's-temp-lr'},
-        {name: 'Bedroom', device: 's-temp-bed'},
-        {name: 'Kitchen', device : 's-temp-kit'},
-        {name: 'Bathroom', device : 's-temp-bath'}
+        {name: 'Living Room', device : 's-temp-lr', data: 'No Data'},
+        {name: 'Bedroom', device: 's-temp-bed', data: 'No Data'},
+        {name: 'Kitchen', device : 's-temp-kit', data: 'No Data'},
+        {name: 'Bathroom', device : 's-temp-bath', data: 'No Data'}
     ];
 
     $scope.TempSensors = TempSensors;
 
     var successCallback = function(data) {
-//    console.log('Got back ' + data['device-id']);
         var device = $.grep($scope.TempSensors, function(e){return e.device == data['device-id']})[0];
-        if(typeof data == 'string') {
-            console.log(' Got This!: ' + data);
-            device.data = data;
-        } else {
+        if(device)
+        {
             device.data = parseInt(data.status)/10.0;
         }
     }
@@ -69,3 +66,36 @@ sodecApp.controller('occupancyController', function($scope, deviceService) {
         deviceService.getData(successCallback, $scope.OccSensors[i]);
     }
 });
+
+
+sodecApp.controller('lightingController', function($scope, deviceService) {
+    var LightSensors = [
+        {name: 'Living Room', device : 's-light-chandelier-1B'},
+        {name: 'Bedroom', device: 's-light-bedroom-uplight-6A'},
+        {name: 'Kitchen', device : 's-light-kitchen-uplight-3A'},
+        {name: 'Bathroom', device : 's-light-bathroom-ambient-4A'}
+    ];
+
+    $scope.LightSensors = LightSensors;
+
+    var successCallback = function(data) {
+//    console.log('Got back ' + data['device-id']);
+      var device = $.grep($scope.LightSensors, function(e){return e.device == data['device-id']})[0];
+      if(device)
+      {
+            device.data = parseInt(data.status)/10.0;
+      }
+    }
+
+    for(var i = 0; i < LightSensors.length; i++) {
+        deviceService.getData(successCallback, $scope.LightSensors[i]);
+    }
+});
+
+sodecApp.filter('format', function() {
+   return function(condition, ifTrue, ifFalse)
+   {
+      return condition ? ifTrue : ifFalse;
+   };
+});
+  
